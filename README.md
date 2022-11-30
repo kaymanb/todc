32bedfc17d29b5b6fe0b8a9ddb558624a102cdc9
Kayman Brusse <kaymanbrusse@gmail.com>
2022-11-29 22:13:04 -0500

    Add IntegerRegister & update README with todo list

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Theory of Distributed Computing
 
 A library of objects and algorithms in Rust. 
+
+### TODO
+
+- Some sort of implementation test
+- Shared snapshot
+- `StringRegister`
+- `Register<T>` 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,45 @@
+use std::sync::atomic::{AtomicU8, Ordering};
+
+pub struct IntegerRegister {
+    data: AtomicU8,
+}
+
+impl IntegerRegister {
+    
+    fn new(value: u8) -> Self {
+        Self {
+           data: AtomicU8::new(value) 
+        }
+    }
+
+    fn read(&self) -> u8 {
+        self.data.load(Ordering::Relaxed)
+    }
+
+    fn write(&self, value: u8) -> () {
+        self.data.store(value, Ordering::Relaxed)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_new() {
+        IntegerRegister::new(0);
+    }
+    
+    #[test]
+    fn test_read() {
+        let register = IntegerRegister::new(0);
+        assert_eq!(0, register.read());
+    }
+    
+    #[test]
+    fn test_write() {
+        let register = IntegerRegister::new(0);
+        register.write(1);
+        assert_eq!(1, register.read());
     }
 }
