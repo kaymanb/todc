d7cdb79e4be0bd5feb81b48d3608f0e3b7a88ae6
Kayman Brusse <kaymanbrusse@gmail.com>
2022-12-06 23:14:53 -0500

    Working example using UnboundedAtomicSnapshot

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
 use std::sync::Arc;
 use std::thread;
-//use std::time::Duration;
-use todc::register::{AtomicRegister, Register};
+
+use todc::snapshot::{Snapshot, UnboundedAtomicSnapshot};
 
 fn main() {
-    // let num_threads = 3;
-    // let registers = Arc::new(vec![AtomicRegister::new(0); num_threads]);
-
-    const SIZE: usize = 3;
-    let registers = Arc::new([(); SIZE].map(|_| AtomicRegister::new(0)));
+    const SIZE: usize = 10;
+    let snapshot: Arc<UnboundedAtomicSnapshot<usize, SIZE>> = Arc::new(UnboundedAtomicSnapshot::new(0));
 
     for i in 0..SIZE {
-        let registers = Arc::clone(&registers);
+        let snapshot = Arc::clone(&snapshot);
         thread::spawn(move || {
-            registers[i].write(1);
+            snapshot.update(i, i);
         });
     }
 
-    for i in 0..SIZE {
-        println!("Read {} from register {}", registers[i].read(), i);
-    }
+    println!("Read {:?} from snapshot", snapshot.scan());
 }
