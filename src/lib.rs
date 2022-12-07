3c5e9e33c1d3bf83f7dc0e4244af1981156d65a9
Kayman Brusse <kaymanbrusse@gmail.com>
2022-12-06 23:41:02 -0500

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! An libary of algorithms from the theory of distributed computing, written in Rust.
+//! A libary of algorithms from the theory of distributed computing, written in Rust.
 
 pub mod register;
 pub mod snapshot;
