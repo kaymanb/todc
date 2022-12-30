2b1bb9abed75494e5c928f907523253b1f658093
Kayman Brusse <kaymanbrusse@gmail.com>
2022-12-30 09:46:02 -0500

    WIP: Messing around with how to implement binary tree

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-use std::sync::Arc;
-use std::thread;
+// use std::sync::Arc;
+// use std::thread;
 
-use todc::snapshot::{aad_plus_93::BoundedAtomicSnapshot, Snapshot};
+// use todc::snapshot::{aad_plus_93::BoundedAtomicSnapshot, Snapshot};
+
+// fn main() {
+//     const SIZE: usize = 10;
+//     let snapshot: Arc<BoundedAtomicSnapshot<usize, SIZE>> = Arc::new(BoundedAtomicSnapshot::new(0));
+
+//     for i in 0..SIZE {
+//         let snapshot = Arc::clone(&snapshot);
+//         thread::spawn(move || {
+//             snapshot.update(i, i + 1);
+//         });
+//     }
+
+//     println!("Read {:?} from snapshot", snapshot.scan(0));
+// }
+
+struct Node<T> {
+    value: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>
+}
 
 fn main() {
-    const SIZE: usize = 10;
-    let snapshot: Arc<BoundedAtomicSnapshot<usize, SIZE>> = Arc::new(BoundedAtomicSnapshot::new(0));
-
-    for i in 0..SIZE {
-        let snapshot = Arc::clone(&snapshot);
-        thread::spawn(move || {
-            snapshot.update(i, i + 1);
-        });
-    }
-
-    println!("Read {:?} from snapshot", snapshot.scan(0));
+    let bst = Node {
+        value: 0,
+        left: Some(Box::new(Node { value: 1, left: None, right: None })),
+        right: None
+    };
+    print!("{}", bst.value)
 }
