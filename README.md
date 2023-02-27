6d60494d53475026ac0aab0fc2523f025ab97804
Kayman Brusse <kayman.brusse@heliolytics.com>
2023-02-26 19:18:48 -0500

    Add benchmarks for snapshot implementations

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 
 A library of algorithms used in theory of distributed computing, written in Rust. 
 
-## Integration Tests
+## Tests
+
+```
+cargo test
+```
+
+### Testing shared-memory
 
 Run with the help of [loom](https://github.com/tokio-rs/loom):
 ```
 LOOM_MAX_PREEMPTIONS=3 RUSTFLAGS="--cfg loom" cargo test --test main --release snapshot
+```
+
+## Benchmarks
+
+Run with the help of [criterion](https://github.com/bheisler/criterion.rs)
+```
+cargo criterion
 ```
--- benches/linearizability.rs
+++ benches/linearizability.rs
@@ -1,13 +1,15 @@
 use core::time::Duration;
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+
+use criterion::{criterion_main, criterion_group, BatchSize, Criterion};
+
 use todc::linearizability::WLGChecker;
 use utils::specifications::etcd::{history_from_log, EtcdSpecification};
 
-const FILE: &str = "benches/linearizability_wlg_checker/etcd_log_005.log";
+const LOG_FILE: &str = "benches/linearizability/etcd_log_005.log";
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let history = history_from_log(FILE.to_owned());
-    c.bench_function("Check etcd linearizability", |b| {
+    let history = history_from_log(LOG_FILE.to_owned());
+    c.bench_function("WLGChecker - check linearizability of etcd log", |b| {
         b.iter_batched(
             || history.clone(),
             |history| WLGChecker::is_linearizable(EtcdSpecification, history),
@@ -17,8 +19,8 @@ fn criterion_benchmark(c: &mut Criterion) {
 }
 
 criterion_group! {
-    name = benches;
+    name = wlg_checker;
     config = Criterion::default().measurement_time(Duration::from_secs(6));
     targets = criterion_benchmark
 }
-criterion_main!(benches);
+criterion_main! { wlg_checker }
