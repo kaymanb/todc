9a46ab7861daf21b27c3206fed8806d3dd022494
Kayman Brusse <kayman.brusse@heliolytics.com>
2023-03-28 21:59:43 -0400

    Add integration tests for UnboundedAtomicSnapshot

--- tests/snapshot/aad_plus_93.rs
+++ tests/snapshot/aad_plus_93.rs
@@ -8,7 +8,58 @@ use super::{RecordingSnapshot, TimedAction};
 
 const NUM_THREADS: usize = 3;
 
-mod unbounded_snapshot {
+// TODO: Reduce code duplication between these tests...
+mod unbounded_atomic_snapshot {
+    use todc::snapshot::aad_plus_93::UnboundedAtomicSnapshot;
+
+    use super::*;
+
+    type ActionUnderTest = TimedAction<u8, NUM_THREADS>;
+    type SnapshotUnderTest =
+        RecordingSnapshot<NUM_THREADS, UnboundedAtomicSnapshot<NUM_THREADS>>;
+
+    #[test]
+    fn test_one_shot_correctness() {
+        loom::model(|| {
+            let actions: Arc<Mutex<Vec<ActionUnderTest>>> = Arc::new(Mutex::new(vec![]));
+            let mut handles = Vec::new();
+            let snapshot: Arc<SnapshotUnderTest> = Arc::new(RecordingSnapshot::new());
+
+            for i in 0..NUM_THREADS {
+                let actions = actions.clone();
+                let snapshot = snapshot.clone();
+                handles.push(thread::spawn(move || {
+                    let (update_call, update_resp) = snapshot.update(i, (i + 1) as u8);
+                    let (scan_call, scan_resp) = snapshot.scan(i);
+                    for action in [update_call, update_resp, scan_call, scan_resp] {
+                        let mut actions = actions.lock().unwrap();
+                        actions.push(action);
+                    }
+                }));
+            }
+
+            for handle in handles {
+                handle.join().unwrap();
+            }
+
+            let actions = &mut *actions.lock().unwrap();
+            actions.sort_by(|a, b| a.happened_at.cmp(&b.happened_at));
+            let history = History::from_actions(
+                actions
+                    .iter()
+                    .map(|ta| (ta.process, ta.action.clone()))
+                    .collect(),
+            );
+            // TODO: Check that all possible histories are being generated...
+            assert!(WLGChecker::is_linearizable(
+                SnapshotSpecification::init(),
+                history
+            ));
+        });
+    }
+}
+
+mod unbounded_mutex_snapshot {
     use todc::snapshot::aad_plus_93::UnboundedMutexSnapshot;
 
     use super::*;
@@ -67,7 +118,6 @@ mod bounded_snapshot {
         RecordingSnapshot<NUM_THREADS, BoundedSnapshot<Option<usize>, NUM_THREADS>>;
 
     #[test]
-    // TODO: Reduce code duplication between these tests.
     fn test_one_shot_correctness() {
         loom::model(|| {
             let actions: Arc<Mutex<Vec<ActionUnderTest>>> = Arc::new(Mutex::new(vec![]));
