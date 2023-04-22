788c85f1cc74f9f14edcd67e8ca37c03c1c61827
Kayman Brusse <kayman.brusse@heliolytics.com>
2023-04-22 16:06:32 -0400

    Update internal values during POST to /register/local

--- todc-mp/src/register.rs
+++ todc-mp/src/register.rs
@@ -4,7 +4,6 @@
 use std::fmt::Debug;
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::atomic::AtomicU32;
 use std::sync::{Arc, Mutex};
 
 use bytes::{Buf, Bytes};
@@ -13,34 +12,38 @@ use hyper::service::Service;
 use hyper::{body::Incoming as IncomingBody, Method, Request, Response};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
-type RegisterResponse = Response<Full<Bytes>>;
-type GenericError = Box<dyn std::error::Error + Send + Sync>;
-
 fn mk_response(
     s: String,
 ) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error + Send + Sync>> {
     Ok(Response::builder().body(Full::new(Bytes::from(s))).unwrap())
 }
 
-#[derive(Default, Deserialize, Serialize)]
-struct LocalValue<T: Default + Send> {
-    label: AtomicU32,
-    value: Mutex<T>,
+#[derive(Debug, Default, Deserialize, PartialEq, Eq, PartialOrd, Ord, Serialize)]
+struct LocalValue<T: Default + Ord + Send> {
+    label: u32,
+    value: T,
 }
 
-pub struct AtomicRegister<T: Default + Send> {
-    local: Arc<LocalValue<T>>,
+#[derive(Clone)]
+pub struct AtomicRegister<T: Default + Ord + Send> {
+    local: Arc<Mutex<LocalValue<T>>>,
 }
 
-impl<T: Default + Send> AtomicRegister<T> {
+impl<T: Default + Ord + Send> AtomicRegister<T> {
     pub fn new() -> Self {
         Self {
-            local: Arc::new(LocalValue::default()),
+            local: Arc::new(Mutex::new(LocalValue::default())),
         }
     }
 }
 
-impl<T: Debug + Default + DeserializeOwned + Send + Serialize + 'static>
+impl<T: Default + Ord + Send> Default for AtomicRegister<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Debug + Default + DeserializeOwned + Ord + Send + Serialize + 'static>
     Service<Request<IncomingBody>> for AtomicRegister<T>
 {
     type Response = Response<Full<Bytes>>; // RegisterResponse
@@ -60,7 +63,13 @@ impl<T: Debug + Default + DeserializeOwned + Send + Serialize + 'static>
             (&Method::POST, "/register/local") => Box::pin(async move {
                 let body = req.collect().await?.aggregate();
                 let other: LocalValue<T> = serde_json::from_reader(body.reader())?;
-                let serialized = serde_json::to_string(&other);
+
+                let mut local = local.lock().unwrap();
+                if other > *local {
+                    *local = other
+                };
+
+                let serialized = serde_json::to_string(&*local);
                 match serialized {
                     Ok(value) => mk_response(value),
                     Err(err) => Err(err.into()),
@@ -73,4 +82,24 @@ impl<T: Debug + Default + DeserializeOwned + Send + Serialize + 'static>
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    mod local_value {
+        use super::*;
+
+        #[test]
+        fn orders_by_label_first() {
+            let first = LocalValue { label: 0, value: 1 };
+            let second = LocalValue { label: 1, value: 0 };
+            assert!(first < second)
+        }
+
+        #[test]
+        fn orders_by_value_if_labels_match() {
+            let first = LocalValue { label: 0, value: 0 };
+            let second = LocalValue { label: 0, value: 1 };
+            assert!(first < second)
+        }
+    }
+}
--- todc-mp/tests/register.rs
+++ todc-mp/tests/register.rs
@@ -15,11 +15,13 @@ use common::{fetch_url, post_url};
 async fn serve_register() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let addr = (IpAddr::from(Ipv4Addr::UNSPECIFIED), 9999);
     let listener = TcpListener::bind(addr).await?;
+    let register = AtomicRegister::<u32>::new();
     loop {
         let (stream, _) = listener.accept().await?;
+        let register = register.clone();
         tokio::task::spawn(async move {
             if let Err(err) = http1::Builder::new()
-                .serve_connection(stream, AtomicRegister::<u32>::new())
+                .serve_connection(stream, register)
                 .await
             {
                 println!("error serving connection: {:?}", err);
@@ -70,7 +72,7 @@ mod local {
         use super::*;
 
         #[test]
-        fn responds_with_success_if_valid_body() {
+        fn responds_with_success_if_valid_request() {
             let mut sim = Builder::new().build();
             sim.host("server1", move || serve_register());
 
@@ -86,18 +88,84 @@ mod local {
         }
 
         #[test]
-        fn responds_by_echoing_request_body() {
+        fn returns_value_with_larger_label() {
             let mut sim = Builder::new().build();
             sim.host("server1", move || serve_register());
 
             sim.client("client", async move {
                 let url = Uri::from_static("http://server1:9999/register/local");
-                let value = json!({"value": 0, "label": 0});
-                let response = post_url(url, value.to_string()).await.unwrap();
+                let larger = json!({"value": 0, "label": 1});
+                let response = post_url(url, larger.to_string()).await.unwrap();
 
                 let body_bytes = response.collect().await?.to_bytes();
                 let body = std::str::from_utf8(&body_bytes)?;
-                assert_eq!(body, value.to_string());
+                assert_eq!(body, larger.to_string());
+                Ok(())
+            });
+
+            sim.run().unwrap();
+        }
+
+        #[test]
+        fn returns_larger_value_if_labels_are_equal() {
+            let mut sim = Builder::new().build();
+            sim.host("server1", move || serve_register());
+
+            sim.client("client", async move {
+                let url = Uri::from_static("http://server1:9999/register/local");
+                let larger = json!({"value": 1, "label": 0});
+                let response = post_url(url, larger.to_string()).await.unwrap();
+
+                let body_bytes = response.collect().await?.to_bytes();
+                let body = std::str::from_utf8(&body_bytes)?;
+                assert_eq!(body, larger.to_string());
+                Ok(())
+            });
+
+            sim.run().unwrap();
+        }
+
+        #[test]
+        fn changes_internal_value_if_request_has_larger_label() {
+            let mut sim = Builder::new().build();
+            sim.host("server1", move || serve_register());
+
+            sim.client("client", async move {
+                let url = Uri::from_static("http://server1:9999/register/local");
+                let larger = json!({"value": 0, "label": 1});
+                post_url(url.clone(), larger.to_string()).await.unwrap();
+
+                // Submit GET request to check internal value
+                let response = fetch_url(url).await.unwrap();
+                let body_bytes = response.collect().await?.to_bytes();
+                let body = std::str::from_utf8(&body_bytes)?;
+                assert_eq!(body, larger.to_string());
+                Ok(())
+            });
+
+            sim.run().unwrap();
+        }
+
+        #[test]
+        fn does_not_change_internal_value_if_request_has_smaller_label() {
+            let mut sim = Builder::new().build();
+            sim.host("server1", move || serve_register());
+
+            sim.client("client", async move {
+                let url = Uri::from_static("http://server1:9999/register/local");
+                // POST an initial value with larger label
+                let larger = json!({"value": 0, "label": 2});
+                post_url(url.clone(), larger.to_string()).await.unwrap();
+
+                // POST a second value with smaller label
+                let smaller = json!({"value": 0, "label": 1});
+                post_url(url.clone(), smaller.to_string()).await.unwrap();
+
+                // Submit GET request to check internal value
+                let response = fetch_url(url).await.unwrap();
+                let body_bytes = response.collect().await?.to_bytes();
+                let body = std::str::from_utf8(&body_bytes)?;
+                assert_eq!(body, larger.to_string());
                 Ok(())
             });
 
