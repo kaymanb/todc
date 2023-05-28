279ee939acfee366786fc87813391888de81f1a4
Kayman Brusse <kaymanbrusse@gmail.com>
2023-05-28 09:35:59 -0400

    WIP: Infer neighboring nodes from K8s env vars

--- todc-net/examples/atomic-register-docker-minikube/src/main.rs
+++ todc-net/examples/atomic-register-docker-minikube/src/main.rs
@@ -6,10 +6,9 @@ use http_body_util::combinators::BoxBody;
 use http_body_util::{BodyExt, Full};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Method, Request, Response, StatusCode};
-// use todc_net::atomic::register::abd_95::AtomicRegister;
+use hyper::{Method, Request, Response, StatusCode, Uri};
+use todc_net::atomic::register::abd_95::AtomicRegister;
 use tokio::net::TcpListener;
-
 
 async fn router(
     req: Request<hyper::body::Incoming>,
@@ -27,26 +26,40 @@ async fn router(
 
 /// Returns a vector containing the URL of all neighboring
 /// AtomicRegister instances in the local cluster.
-fn find_neighbors() {
+fn find_neighbors() -> Vec<Uri> {
+    let pod_name =
+        env::var("POD_NAME").expect("environmental variable 'POD_NAME' should be set by K8s");
 
-    let pod_name = env::var("POD_NAME")
-        .expect("environmental variable 'POD_NAME' should be set by K8s");
+    let (app_name, ordinal_str) = pod_name
+        .rsplit_once('-')
+        .expect("pod name should be of the format {APP_NAME}-{ORDINAL}");
+    println!("App Name: {app_name:?}");
+
+    let ordinal: u32 = ordinal_str.parse().expect("Ordinal should be a valid u32");
+    println!("Ordinal: {ordinal:?}");
 
     let num_replicas: u32 = env::var("NUM_REPLICAS")
         .expect("environmental variable 'NUM_RECORDS' should be set by K8s")
         .parse()
-        .expect("environmental variable 'NUM_RECORDS' should be stringified u32");
-
-    println!("Name: {pod_name:?}");
+        .expect("environmental variable 'NUM_RECORDS' should be valid u32");
     println!("Number of Replicas: {num_replicas:?}");
-    
+
+    (0..num_replicas)
+        .filter(|i| i != &ordinal)
+        .map(|i| {
+            format!("http://{app_name}-{i}.svc.cluster.local")
+                .parse()
+                .unwrap()
+        })
+        .collect()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr: SocketAddr = ([0, 0, 0, 0], 3000).into();
 
-    find_neighbors();
+    let neighbors = find_neighbors();
+    let _register: AtomicRegister<String> = AtomicRegister::new(neighbors);
 
     let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
