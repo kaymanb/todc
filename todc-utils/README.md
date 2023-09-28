52ca4f90994e25b785d39060eff0ad011899e2f1
Kayman Brusse <kaymanbrusse@gmail.com>
2023-09-27 23:12:28 -0400

    Update todc-utils README and documentation examples

--- todc-utils/README.md
+++ todc-utils/README.md
@@ -7,20 +7,115 @@ Utilities for building and testing distributed algorithms.
 
 ## Examples
 
-Determine if a history of operations performed on some shared-object, like 
-[`etcd`](https://etcd.io/), is actually linearizable. See `todc-utils/tests/etcd.rs`
-for more details.
+Consider the following sequential specification for a register containing 
+`u32` values.
 
 ```rs
-use todc_utils::linearizability::WGLChecker;
-use todc_utils::specifications::etcd::{history_from_log, EtcdSpecification};
+use todc_utils::specifications::Specification;
 
-// Define a linearizability checker for an etcd (compare-and-swap) object.
-type EtcdChecker = WGLChecker<EtcdSpecification>;
+#[derive(Copy, Clone, Debug)]
+enum RegisterOp {
+    Read(Option<u32>),
+    Write(u32),
+}
 
-// Create a history of operations based on log output.
-let history = history_from_log("todc-utils/tests/linearizability/etcd/etcd_001.log")
+use RegisterOp::{Read, Write};
 
-// Assert that the history of operations is actually linearizable.
-assert!(EtcdChecker::is_linearizable(history));
+struct RegisterSpec;
+
+impl Specification for RegisterSpec {
+    type State = u32;
+    type Operation = RegisterOp;
+    
+    fn init() -> Self::State {
+        0
+    }
+
+    fn apply(operation: &Self::Operation, state: &Self::State) -> (bool, Self::State) {
+        match operation {
+            // A read is valid if the value returned is equal to the
+            // current state. Reads always leave the state unchanged.
+            Read(value) => match value {
+                Some(value) => (value == state, *state),
+                None => (false, *state)
+            },
+            // Writes are always valid, and update the state to be
+            // equal to the value being written.
+            Write(value) => (true, *value),
+        }
+    }
+}
+```    
+
+Using the `Action::Call` and `Action::Response` types, we can model read
+and write operations as follows:
+
+* The call of a read operation is modeled by `Call(Read(None))` and a
+  response containing the value `x` is modeled by `Response(Read(Some(x)))`.
+  We are use an `Option` because the value being read cannot be known until 
+  the register responds.
+* Similarily, the call of a write operation with the value `y` is modeled
+  by `Call(Write(y))` and the response is modeled by `Response(Write(y))`.
+
+Next, we can define a linearizability for this specification, and check some
+histories.
+
+```rs
+use todc_utils::linearizability::{WGLChecker, history::{History, Action::{Call, Response}}};
+
+type RegisterChecker = WGLChecker<RegisterSpec>;
+
+// A history of sequantial operations is always linearizable.
+// PO |------|          Write(0)
+// P1          |------| Read(Some(0))
+let history = History::from_actions(vec![
+    (0, Call(Write(0))),
+    (0, Response(Write(0))),
+    (1, Call(Read(None))),
+    (1, Response(Read(Some(0)))),
+]);
+assert!(RegisterChecker::is_linearizable(history));
+
+// Concurrent operations might not be linearized
+// in the order in which they are called.
+// PO |---------------| Write(0)
+// P1  |--------------| Write(1)
+// P2    |---|          Read(Some(1))
+// P3           |---|   Read(Some(0))
+let history = History::from_actions(vec![
+    (0, Call(Write(0))),
+    (1, Call(Write(1))),
+    (2, Call(Read(None))),
+    (2, Response(Read(Some(1)))),
+    (3, Call(Read(None))),
+    (3, Response(Read(Some(0)))),
+    (0, Response(Write(0))),
+    (1, Response(Write(1))),
+]);
+assert!(RegisterChecker::is_linearizable(history));
+
+// A sequentially consistent history is **not**
+// necessarily linearizable.
+// PO |---|             Write(0)
+// P1 |---|             Write(1)
+// P2       |---|       Read(Some(1))
+// P3             |---| Read(Some(0))
+let history = History::from_actions(vec![
+    (0, Call(Write(0))),
+    (1, Call(Write(1))),
+    (0, Response(Write(0))),
+    (1, Response(Write(1))),
+    (2, Call(Read(None))),
+    (2, Response(Read(Some(1)))),
+    (3, Call(Read(None))),
+    (3, Response(Read(Some(0)))),
+]);
+assert!(!RegisterChecker::is_linearizable(history));
 ```
+
+For examples of using `WGLChecker` to check the linearizability of more
+complex histories, see
+[`todc-mem/tests/snapshot/common.rs`](https://github.com/kaymanb/todc/blob/main/todc-mem/tests/snapshot/common.rs)
+or
+[`todc-utils/tests/linearizability/etcd.rs`](https://github.com/kaymanb/todc/blob/main/todc-utils/tests/linearizability/etcd.rs).
+
--- todc-utils/src/linearizability.rs
+++ todc-utils/src/linearizability.rs
@@ -35,7 +35,7 @@ pub mod history;
 ///
 /// #[derive(Copy, Clone, Debug)]
 /// enum RegisterOp {
-///     Read(u32),
+///     Read(Option<u32>),
 ///     Write(u32),
 /// }
 ///
@@ -53,35 +53,57 @@ pub mod history;
 ///
 ///     fn apply(operation: &Self::Operation, state: &Self::State) -> (bool, Self::State) {
 ///         match operation {
-///             Read(value) => (value == state, *state),
+///             // A read is valid if the value returned is equal to the
+///             // current state. Reads always leave the state unchanged.
+///             Read(value) => match value {
+///                 Some(value) => (value == state, *state),
+///                 None => (false, *state)
+///             },
+///             // Writes are always valid, and update the state to be
+///             // equal to the value being written.
 ///             Write(value) => (true, *value),
 ///         }
 ///     }
 /// }
 /// ```    
 ///
-/// We can confirm that a history where two processes sequentially perform reads
-/// and writes is in fact linearizable.
+/// Using the [`Action::Call`](history::Action::Call) and
+/// [`Action::Response`](history::Action::Response) types, we can model read
+/// and write operations as follows:
+/// * The call of a read operation is modeled by `Call(Read(None))` and a
+///   response containing the value `x` is modeled by `Response(Read(Some(x)))`.
+///   We are required to use an [`Option`] because the value being read cannot
+///   be known until the register responds.
+/// * Similarily, the call of a write operation with the value `y` is modeled
+///   by `Call(Write(y))` and the response is modeled by `Response(Write(y))`.
 ///
+/// Next, we can define a linearizability for this specification, and check some
+/// histories.
 /// ```
 /// # use todc_utils::specifications::Specification;
 /// # #[derive(Copy, Clone, Debug)]
 /// # enum RegisterOp {
-/// #     Read(u32),
+/// #     Read(Option<u32>),
 /// #     Write(u32),
 /// # }
-/// # use RegisterOp::*;
+/// # use RegisterOp::{Read, Write};
 /// # struct RegisterSpec;
 /// # impl Specification for RegisterSpec {
 /// #     type State = u32;
 /// #     type Operation = RegisterOp;
-/// #     
 /// #     fn init() -> Self::State {
 /// #         0
 /// #     }
 /// #     fn apply(operation: &Self::Operation, state: &Self::State) -> (bool, Self::State) {
 /// #         match operation {
-/// #             Read(value) => (value == state, *state),
+/// #             // A read is valid if the value returned is equal to the
+/// #             // current state. Reads always leave the state unchanged.
+/// #             Read(value) => match value {
+/// #                 Some(value) => (value == state, *state),
+/// #                 None => (false, *state)
+/// #             },
+/// #             // Writes are always valid, and update the state to be
+/// #             // equal to the value being written.
 /// #             Write(value) => (true, *value),
 /// #         }
 /// #     }
@@ -90,51 +112,59 @@ pub mod history;
 ///
 /// type RegisterChecker = WGLChecker<RegisterSpec>;
 ///
+/// // A history of sequantial operations is always linearizable.
+/// // PO |------|          Write(0)
+/// // P1          |------| Read(Some(0))
 /// let history = History::from_actions(vec![
-///     (0, Call(Write(1))),
-///     (0, Response(Write(1))),
-///     (1, Call(Read(1))),
-///     (1, Response(Read(1))),
+///     (0, Call(Write(0))),
+///     (0, Response(Write(0))),
+///     (1, Call(Read(None))),
+///     (1, Response(Read(Some(0)))),
 /// ]);
 /// assert!(RegisterChecker::is_linearizable(history));
-/// ```    
 ///
-/// If the second process performs a read that returns an invalid value, we can
-/// check that the corresponding history is **not** linearizable as well.
-///
-/// ```
-/// # use todc_utils::specifications::Specification;
-/// # #[derive(Copy, Clone, Debug)]
-/// # enum RegisterOp {
-/// #     Read(u32),
-/// #     Write(u32),
-/// # }
-/// # use RegisterOp::*;
-/// # struct RegisterSpec;
-/// # impl Specification for RegisterSpec {
-/// #     type State = u32;
-/// #     type Operation = RegisterOp;
-/// #     
-/// #     fn init() -> Self::State {
-/// #         0
-/// #     }
-/// #     fn apply(operation: &Self::Operation, state: &Self::State) -> (bool, Self::State) {
-/// #         match operation {
-/// #             Read(value) => (value == state, *state),
-/// #             Write(value) => (true, *value),
-/// #         }
-/// #     }
-/// # }
-/// # use todc_utils::linearizability::{WGLChecker, history::{History, Action::{Call, Response}}};
-/// # type RegisterChecker = WGLChecker<RegisterSpec>;
+/// // Concurrent operations might not be linearized
+/// // in the order in which they are called.
+/// // PO |--------------|  Write(0)
+/// // P1  |--------------| Write(1)
+/// // P2    |---|          Read(Some(1))
+/// // P3           |---|   Read(Some(0))
 /// let history = History::from_actions(vec![
-///     (0, Call(Write(1))),
-///     (0, Response(Write(1))),
-///     (1, Call(Read(42))),
-///     (1, Response(Read(42))),
+///     (0, Call(Write(0))),
+///     (1, Call(Write(1))),
+///     (2, Call(Read(None))),
+///     (2, Response(Read(Some(1)))),
+///     (3, Call(Read(None))),
+///     (3, Response(Read(Some(0)))),
+///     (0, Response(Write(0))),
+///     (1, Response(Write(1))),
+/// ]);
+/// assert!(RegisterChecker::is_linearizable(history));
+///
+/// // A sequentially consistent history is **not**
+/// // necessarily linearizable.
+/// // PO |---|             Write(0)
+/// // P1 |---|             Write(1)
+/// // P2       |---|       Read(Some(1))
+/// // P3             |---| Read(Some(0))
+/// let history = History::from_actions(vec![
+///     (0, Call(Write(0))),
+///     (1, Call(Write(1))),
+///     (0, Response(Write(0))),
+///     (1, Response(Write(1))),
+///     (2, Call(Read(None))),
+///     (2, Response(Read(Some(1)))),
+///     (3, Call(Read(None))),
+///     (3, Response(Read(Some(0)))),
 /// ]);
 /// assert!(!RegisterChecker::is_linearizable(history));
-/// ```
+/// ```    
+///   
+/// For examples of using [`WGLChecker`] to check the linearizability of more
+/// complex histories, see
+/// [`todc-mem/tests/snapshot/common.rs`](https://github.com/kaymanb/todc/blob/main/todc-mem/tests/snapshot/common.rs)
+/// or
+/// [`todc-utils/tests/linearizability/etcd.rs`](https://github.com/kaymanb/todc/blob/main/todc-utils/tests/linearizability/etcd.rs).
 ///
 /// # Implementations in Other Languages
 ///
